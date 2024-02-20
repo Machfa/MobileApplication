75ddbcbd56deca8f64bf7d24445202538cf1588f
Kettroussi-aniss <a.kettroussi@esi-sba.dz>
2024-02-20 20:14:25 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Machfaa',
+        title: 'Machfa',
         theme: ThemeData(
           useMaterial3: true,
         ),
