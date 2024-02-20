8bb1b5508078dbca9861c60b762cf75f1690feab
Kettroussi-aniss <a.kettroussi@esi-sba.dz>
2024-02-20 19:42:01 +0100

    apdate

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Machfa',
+        title: 'Machfaa',
         theme: ThemeData(
           useMaterial3: true,
         ),
